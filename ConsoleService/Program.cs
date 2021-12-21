6e40672be4e6c8f18379d4abdf6c2252a1f0e864
GREGOR ČERNAK <gregor.cernak@as-it.si>
2021-12-21 15:12:46 +0100

    Initial commit

---
+++ ConsoleService/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PuppeteerSharp;
+using PuppeteerSharp.Media;
+using Razor.Templates.ViewModels;
+using Razor.Templating.Core;
+
+Console.WriteLine("Hello, World!");
+
+var model = new InvoiceViewModel
+{
+    CreatedAt = DateTime.Now,
+    Due = DateTime.Now.AddDays(10),
+    Id = 12533,
+    AddressLine = "Jumpy St. 99",
+    City = "Trampoline",
+    ZipCode = "22-113",
+    CompanyName = "Jumping Rabbit Co.",
+    PaymentMethod = "Check",
+    Items = new List<InvoiceItemViewModel>
+                {
+                    new InvoiceItemViewModel("Website design", 621.99m),
+                    new InvoiceItemViewModel("Website creation", 1231.99m)
+                }
+};
+
+var html = await RazorTemplateEngine.RenderAsync("~/Views/Invoice.cshtml", model);
+
+Console.WriteLine(html);
+
+using (StreamWriter outputFile = new StreamWriter(Path.Combine("Invoice.html")))
+{
+        outputFile.WriteLine(html);
+}
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+await using var page = await browser.NewPageAsync();
+await page.EmulateMediaTypeAsync(MediaType.Screen);
+await page.SetContentAsync(html);
+await page.PdfAsync("Invoice.pdf", new PdfOptions
+{
+    Format = PaperFormat.A4,
+    PrintBackground = true
+});
